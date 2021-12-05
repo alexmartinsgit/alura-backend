3d559a4c720a28420aeaf6ed27ebd5b033e77f81
Ana <ana@gmail.com>
2021-12-05 12:11:43 -0300

    Modificação Json, inserindo descriçãodo video

--- videos/views.py
+++ videos/views.py
@@ -2,5 +2,5 @@ from django.http import JsonResponse, response
 
 def videos(request):
     if request.method == 'GET':
-        video = {'id': 1, 'titulo': 'Moana', 'url':'www.videos.com'}
+        video = {'id': 1, 'titulo': 'Moana', 'url':'www.videos.com', 'desc': 'Vídeo Infantil' }
         return JsonResponse(video)
