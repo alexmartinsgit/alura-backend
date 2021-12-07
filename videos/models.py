a01a38449638efb2fa37735a7288a368ce242916
Alex Martins <josealex.pm@gmail.com>
2021-12-06 22:23:34 -0300

    Criação dos requirements.txt - dependências

--- videos/models.py
+++ videos/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class videos(models.Model):
+    titulo = models.CharField(max_length=30)
+    descricao = models.CharField(max_length=100)
+    url = models.CharField(max_length=50)
+
